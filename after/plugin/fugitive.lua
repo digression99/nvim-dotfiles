43ed3758be4cd05db4488c10af4816c8da7fff0e
digression99 <digression99@gmail.com>
2023-01-09 17:15:28 +0900

    Commit

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,8 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gp", function()
+	vim.cmd.Git("push")
+end);
+
+vim.keymap.set("n", "<leader>gc", function()
+	vim.cmd.Git("commit")
+end);
