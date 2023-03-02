6283dd078a55cb039f12c2612cee194f4f4a8b9e
digression99 <digression99@gmail.com>
2023-03-02 20:11:48 +0900

    Show hidden files on neotree

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -2,3 +2,12 @@
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>")
 vim.keymap.set("n", "<leader>o", "<cmd>Neotree focus<cr>")
 
+require 'neo-tree'.setup {
+  filesystem = {
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignored = false
+    }
+  }
+}
